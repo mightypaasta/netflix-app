11a105353a1bd7fb9fb0b3dc10c365fa55ff9633
Kamal <itsk1201@gmail.com>
2022-04-12 18:08:50 +0530

    Functioning Rows Added

---
+++ src/Row.js
@@ -0,0 +1,35 @@
+import axios from "./axios";
+import React, { useEffect, useState } from "react";
+import './Row.css'
+
+const  Row = ({title,fetchURL,isLargeRow=false})=>{
+    const [movies,setMovies]=useState([]);
+    const baseURL="https://image.tmdb.org/t/p/original/";
+
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(fetchURL);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData();
+    },[fetchURL])
+    console.log(movies);
+    return(
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img 
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    key={movie.id} 
+                    src={`${baseURL}${isLargeRow?movie.poster_path:movie.backdrop_path}`}
+                    alt={movie.name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+export default Row;
